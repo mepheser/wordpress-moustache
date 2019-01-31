fe15a9de249442cad8131c4a9509fee13a549916
Matthias Epheser <matthias.epheser@indoqa.com>
2019-01-31 19:35:08 +0100

    initial

---
+++ src/wp-mustache.php
@@ -0,0 +1,51 @@
+<?php
+namespace mepheser\wordpress_mustache;
+
+class WP_Mustache {
+
+  protected static $_instance = null;
+
+  private $engine;
+
+  public static function getInstance() {
+    if (self::$_instance == null) {
+      self::$_instance = new self;
+    }
+    return self::$_instance;
+  }
+
+  public static function mapQuery($queryParams, $mapAdditionalFields) {
+    $query = new WP_Query($queryParams);
+    $result = array();
+
+    while ( $query->have_posts() ) {
+      $query->the_post();
+
+      $data = array(
+          'permalink' => get_permalink(),
+          'title' => get_field('title'),
+          'description' => get_field('description')
+      );
+
+      if ($mapAdditionalFields) {
+        $mapAdditionalFields($data);
+      }
+
+      array_push($result, $data);
+    }
+
+    wp_reset_query();
+
+    return $result;
+  }
+
+  protected function __construct() {
+    $this->engine = new Mustache_Engine(array(
+        'loader' => new Mustache_Loader_FilesystemLoader(get_template_directory())
+    ));
+  }
+
+  public function render($template, $data) {
+    echo $this->engine->render($template, $data);
+  }
+}
